4e8a6ad5fc7f8b683ea659afccb96861a9188648
My Web <kylavesfar25@gmail.com>
2022-06-23 10:29:05 +0700

    first commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><div class="text-center">ChatKuy</div></div>
+                    <div class="panel-body" style="height: 360px; overflow: auto;" >
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Kyy</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Mandan</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Kyy</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Mandan</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Kyy</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Mandan</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                        <div class="chat-body" style="margin-top: 10px">
+                            <div class="header">
+                                <strong>Kyy</strong>
+                                <small class="pull-right text-muted"><span class="glyphicon ">2 Min Ago</span></small>
+                            </div>
+                            <div class="chatcolor">
+                                Hallo Apa Kabar
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Ketik Disini ..." name="">
+                        <span class="input-group-btn">
+                            <button class="btn btn-warning btn-md">Kirim</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+<style type="text/css">
+    .chatcolor{
+        background-color: #f0ad4e;
+        padding: 10px;
+        border-radius: 20px;
+        color: #222222
+    }
+</style>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,118 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootswatch: Darkly</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://bootswatch.com/3/darkly/bootstrap.min.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
+      <script src="../bower_components/respond/dest/respond.min.js"></script>
+    <![endif]-->
+    <script>
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23019901-1']);
+      _gaq.push(['_setDomainName', 'bootswatch.com']);
+      _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+     (function() {
+       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+     })();
+
+    </script>
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="../" class="navbar-brand">Bootswatch</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a href="{{ route('chat') }}" class="nav-link">Chat</a>
+                </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+          </ul>
+
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <!-- Forms
+      ================================================== -->
+      <div class="bs-docs-section">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="page-header">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+                  <legend>ChatBox</legend>
+                    @yield('content')
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+    </div>
+
+
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../assets/js/custom.js"></script>
+  </body>
+</html>
